35477700445119da1c94bb7cc50d231ca682c11f
Albrim Sulaj <sulaj@virai.it>
2024-07-07 21:32:45 +0000

    Winrates per side, and counts, --watch on dev

--- backend/services.py
+++ backend/services.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from esports_client import client
 
-
 def get_draft_data(patch):
     """
     Queries and returns draft data for a given patch as a DataFrame.
@@ -73,43 +72,58 @@ def calculate_champion_stats(df, champion_filter=None):
                 'win_count': 0,
                 'blue_side_avg_pick_pos': 0,
                 'red_side_avg_pick_pos': 0,
-                'blue_side_pick_pct': 0,
-                'red_side_pick_pct': 0,
-                'avg_pick_pos': 0
+                'blue_side_pick_count': 0,
+                'red_side_pick_count': 0,
+                'avg_pick_pos': 0,
+                'blue_side_win_count': 0,
+                'red_side_win_count': 0,
+                'blue_side_winrate': 0,
+                'red_side_winrate': 0,
+                'winrate': 0
             }
 
         stats = champion_stats[champion]
         stats['games_played'] += 1
         if is_winner:
             stats['win_count'] += 1
+            if side == 'blue':
+                stats['blue_side_win_count'] += 1
+            elif side == 'red':
+                stats['red_side_win_count'] += 1
 
         if is_pick:
             stats['avg_pick_pos'] += position
             if side == 'blue':
                 stats['blue_side_avg_pick_pos'] += position
-                stats['blue_side_pick_pct'] += 1
+                stats['blue_side_pick_count'] += 1
             else:
                 stats['red_side_avg_pick_pos'] += position
-                stats['red_side_pick_pct'] += 1
+                stats['red_side_pick_count'] += 1
 
-    total_games = len(df)
     for index, row in df.iterrows():
         winner = row['Winner']
+        team1_winner = winner == "1"
+        team2_winner = winner == "2"
+
         for i in range(1, 6):
-            update_stats(row[f'Team1Pick{i}'], 'blue', row[f'Team1Role{i}'], i, True, winner == row['Team1'])
-            update_stats(row[f'Team2Pick{i}'], 'red', row[f'Team2Role{i}'], i, True, winner == row['Team2'])
+            team1_pick = row[f'Team1Pick{i}']
+            team2_pick = row[f'Team2Pick{i}']
+            team1_role = row[f'Team1Role{i}']
+            team2_role = row[f'Team2Role{i}']
+
+            update_stats(team1_pick, 'blue', team1_role, i, True, team1_winner)
+            update_stats(team2_pick, 'red', team2_role, i, True, team2_winner)
 
     for stats in champion_stats.values():
         if stats['games_played'] > 0:
             stats['winrate'] = round((stats['win_count'] / stats['games_played']) * 100, 2)
             stats['avg_pick_pos'] = round(stats['avg_pick_pos'] / stats['games_played'], 1)
-            if stats['blue_side_pick_pct'] > 0:
-                stats['blue_side_avg_pick_pos'] = round(stats['blue_side_avg_pick_pos'] / stats['blue_side_pick_pct'],
-                                                        1)
-                stats['blue_side_pick_pct'] = round((stats['blue_side_pick_pct'] / stats['games_played']) * 100, 3)
-            if stats['red_side_pick_pct'] > 0:
-                stats['red_side_avg_pick_pos'] = round(stats['red_side_avg_pick_pos'] / stats['red_side_pick_pct'], 1)
-                stats['red_side_pick_pct'] = round((stats['red_side_pick_pct'] / stats['games_played']) * 100, 3)
+            if stats['blue_side_pick_count'] > 0:
+                stats['blue_side_avg_pick_pos'] = round(stats['blue_side_avg_pick_pos'] / stats['blue_side_pick_count'], 1)
+                stats['blue_side_winrate'] = round((stats['blue_side_win_count'] / stats['blue_side_pick_count']) * 100, 2)
+            if stats['red_side_pick_count'] > 0:
+                stats['red_side_avg_pick_pos'] = round(stats['red_side_avg_pick_pos'] / stats['red_side_pick_count'], 1)
+                stats['red_side_winrate'] = round((stats['red_side_win_count'] / stats['red_side_pick_count']) * 100, 2)
 
     # Create a DataFrame with the filtered champion's stats if a champion_filter is provided
     if champion_filter:
